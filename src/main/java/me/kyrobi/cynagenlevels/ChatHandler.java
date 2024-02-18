077abc9b6898e58a1d9389a8322047e82dcbdd0a
Kyrobi <lightling123jr@gmail.com>
2024-02-17 20:14:48 -0600

    Send messages somewhere better

--- src/main/java/me/kyrobi/cynagenlevels/ChatHandler.java
+++ src/main/java/me/kyrobi/cynagenlevels/ChatHandler.java
@@ -55,7 +55,16 @@ public class ChatHandler implements Listener {
 
             int levelUpFlag = giveEXP(uuid);
             if(levelUpFlag > 0){
-                Bukkit.broadcastMessage(ChatColor.DARK_AQUA + player.getName() + " advanced to level " + ChatColor.AQUA + levelUpFlag);
+
+                // To prevent spam, only show in server chat if they reach lvl 10+
+                if(levelUpFlag > 9){
+                    Bukkit.broadcastMessage(ChatColor.DARK_AQUA + player.getName() + " advanced to level " + ChatColor.AQUA + levelUpFlag);
+                    player.sendMessage(ChatColor.GRAY + "For more info, use /level");
+                }
+                else{
+                    player.sendMessage(ChatColor.DARK_AQUA + player.getName() + " advanced to level " + ChatColor.AQUA + levelUpFlag);
+                    player.sendMessage(ChatColor.GRAY + "For more info, use /level");
+                }
             }
 
             long currentLevel = getCurrentLevel(uuid);
@@ -95,7 +104,7 @@ public class ChatHandler implements Listener {
             User discordUser = getDiscordUser(UUID.fromString(uuid));
             int levelUpFlag = giveEXP(player.getUniqueId().toString());
             if(levelUpFlag > 0){
-                TextChannel txt = DiscordSRV.getPlugin().getJda().getTextChannelById("415873891857203214");
+                TextChannel txt = DiscordSRV.getPlugin().getJda().getTextChannelById("448488708883218442");
                 txt.sendMessage( discordUser.getAsMention() + " advanced to level " + levelUpFlag).queue();
             }
 
